7befd41622267160c7c39bafd9b78812a5236db9
vi0letchen <violethongjinchen@gmail.com>
2025-11-26 13:58:04 +1300

    Modified UpdateQuality for exercise 2

--- src/GildedRose.Console/Program.cs
+++ src/GildedRose.Console/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 
 namespace GildedRose.Console;
 
@@ -36,78 +37,66 @@ public class Program
 
     public void UpdateQuality()
     {
-        for (var i = 0; i < Items.Count; i++)
-        {
-            if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-            {
-                if (Items[i].Quality > 0)
-                {
-                    if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                    {
-                        Items[i].Quality = Items[i].Quality - 1;
+        foreach (var item in Items)
+          {     
+               if (item.Name != "Sulfuras, Hand of Ragnaros")
+               {
+                    if (item.Name == "Aged Brie")
+                    {   
+                        if (item.SellIn < 0)
+                         {
+                            item.Quality = System.Math.Min(item.Quality + 2, 50);
+                         }
+                         else
+                         {
+                            item.Quality = System.Math.Min(item.Quality + 1, 50);
+                         }
                     }
-                }
-            }
-            else
-            {
-                if (Items[i].Quality < 50)
-                {
-                    Items[i].Quality = Items[i].Quality + 1;
 
-                    if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+                    else if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
                     {
-                        if (Items[i].SellIn < 11)
-                        {
-                            if (Items[i].Quality < 50)
-                            {
-                                Items[i].Quality = Items[i].Quality + 1;
-                            }
-                        }
-
-                        if (Items[i].SellIn < 6)
-                        {
-                            if (Items[i].Quality < 50)
-                            {
-                                Items[i].Quality = Items[i].Quality + 1;
-                            }
-                        }
+                         if (item.SellIn > 10)
+                         {
+                              item.Quality = System.Math.Min(item.Quality + 1, 50);
+                         }
+                         else if (item.SellIn > 5)
+                         {
+                              item.Quality = System.Math.Min(item.Quality + 2, 50);
+                         }
+                         else if (item.SellIn > 0)
+                         {
+                              item.Quality = System.Math.Min(item.Quality + 3, 50);
+                         }
+                         else
+                         {
+                              item.Quality = 0;
+                         }
                     }
-                }
-            }
-
-            if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-            {
-                Items[i].SellIn = Items[i].SellIn - 1;
-            }
-
-            if (Items[i].SellIn < 0)
-            {
-                if (Items[i].Name != "Aged Brie")
-                {
-                    if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                    else if (item.Name.StartsWith("Conjured"))
                     {
-                        if (Items[i].Quality > 0)
-                        {
-                            if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                            {
-                                Items[i].Quality = Items[i].Quality - 1;
-                            }
-                        }
+                         if (item.SellIn < 0)
+                         {
+                              item.Quality = System.Math.Max(item.Quality - 4, 0);
+                         }
+                         else
+                         {
+                              item.Quality = System.Math.Max(item.Quality - 2, 0);
+                         }
                     }
                     else
                     {
-                        Items[i].Quality = Items[i].Quality - Items[i].Quality;
+                         if (item.SellIn < 0)
+                         {
+                              item.Quality = System.Math.Max(item.Quality - 2, 0);
+                         }
+                         else
+                         {
+                                item.Quality = System.Math.Max(item.Quality - 1, 0);
+                         }
                     }
-                }
-                else
-                {
-                    if (Items[i].Quality < 50)
-                    {
-                        Items[i].Quality = Items[i].Quality + 1;
-                    }
-                }
-            }
-        }
+                    item.SellIn -= 1;
+               }
+          }
     }
 }
 
